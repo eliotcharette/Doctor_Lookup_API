80c08940ced6277408199b49cb05653b2e5ecf8b
Guest User <Guest@epicodus-9.local>
2018-08-10 15:09:29 -0700

    Clean up styling

--- src/main.js
+++ src/main.js
@@ -16,11 +16,10 @@ $(document).ready(function() {
       let body = JSON.parse(response);
       console.log(body);
       for (let i = 0; i < body.data.length; i++) {
-        $('.results').prepend(`<li><strong>Doctor: </strong>${body.data[i].profile.first_name} ${body.data[i].profile.last_name}</li><li>Address: ${body.data[i].practices[0].visit_address.street}</li><li>Phone: ${body.data[i].practices[0].phones[0].number}</li><li>Website: ${body.data[i].practices[0].website}</li><br>`);
         if (`${body.data[i].practices[0].accepts_new_patients}` === "true"){
-          $('.results').prepend(`<li>Currently accepting new patients</li>`);
+          $('.results').prepend(`<p><strong>Doctor: </strong>${body.data[i].profile.first_name} ${body.data[i].profile.last_name}<strong> is</strong> currently accpeting new patients</p><li>Address: ${body.data[i].practices[0].visit_address.street}</li><li>Phone: ${body.data[i].practices[0].phones[0].number}</li><li>Website: ${body.data[i].practices[0].website}</li><br>`);
         }else{
-          $('.results').prepend(`<li>Not accepting new patients</li>`);
+          $('.results').prepend(`<p><strong>Doctor: </strong>${body.data[i].profile.first_name} ${body.data[i].profile.last_name} is not current accpeting new patients</p><li>Address: ${body.data[i].practices[0].visit_address.street}</li><li>Phone: ${body.data[i].practices[0].phones[0].number}</li><li>Website: ${body.data[i].practices[0].website}</li><br>`);
         }
       }
       if(body.data.length === 0){
