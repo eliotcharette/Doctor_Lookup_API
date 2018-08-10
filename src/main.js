b948a7b851b71aa523aecd12e6bc01dd62bd7739
Guest User <Guest@epicodus-9.local>
2018-08-10 11:19:01 -0700

    import doctorsearch object from business logic doctor.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-//logic for user interface
-
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { DoctorSearch } from './doctor.js';
