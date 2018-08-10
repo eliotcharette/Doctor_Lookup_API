3b2fdda204e83d6793d5cc41087e22a9cef34cbf
Guest User <Guest@epicodus-9.local>
2018-08-10 12:00:16 -0700

    Add functionality for find doctors first and last name

--- src/main.js
+++ src/main.js
@@ -15,13 +15,9 @@ $(document).ready(function() {
     promise.then(function(response) {
       let body = JSON.parse(response);
       console.log(body);
-      for (let i = 0; i < body.bikes.length; i++) {
-         $('.title').append(`<li>Stolen bike: ${body.bikes[i].title}</li>`);
-         if ( `${body.bikes[i].large_img}` != "null") {
-           $('.title').append(`<img src ='${body.bikes[i].large_img}'/>`);
-
-         }
+      for (let i = 0; i < body.data.length; i++) {
+        $('.title').prepend(`<li>Name: ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}</li>`);
       }
-      });
+    });
   });
 });
