e5dd1134e7f8bfa41bb2fbeb133e1fcb3086b60c
Guest User <Guest@epicodus-9.local>
2018-08-10 13:30:18 -0700

    Add error message

--- src/main.js
+++ src/main.js
@@ -16,9 +16,18 @@ $(document).ready(function() {
       let body = JSON.parse(response);
       console.log(body);
       for (let i = 0; i < body.data.length; i++) {
-        $('.results').prepend(`<li><strong>Doctor: </strong>${body.data[i].profile.first_name} ${body.data[i].profile.last_name}</li><li>Address: ${body.data[i].practices[0].visit_address.street}</li><br>`);
-        // $('.address').prepend(`<li>Address: ${body.data[i].practices[0].visit_address.street}</li>`)
+        $('.results').prepend(`<li><strong>Doctor: </strong>${body.data[i].profile.first_name} ${body.data[i].profile.last_name}</li><li>Address: ${body.data[i].practices[0].visit_address.street}</li><li>Phone: ${body.data[i].practices[0].phones[0].number}</li><li>Website: ${body.data[i].practices[0].website}</li><br>`);
+        if (`${body.data[i].practices[0].accepts_new_patients}` === "true"){
+          $('.results').prepend(`<li>Currently accepting new patients</li>`);
+        }else{
+          $('.results').prepend(`<li>Not accepting new patients</li>`);
+        }
       }
+      if(body.data.length === 0){
+        $('.showErrors').text(`We're sorry, there are no results for your search.`)
+      }
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
   });
 });
